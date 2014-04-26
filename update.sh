a3212bf74fb22aad6cbe05139d721246d6896694
Brent Cook <brent@boundary.com>
2014-04-25 21:55:48 -0500

    s/copy_crypt/copy_crypto/g

--- update.sh
+++ update.sh
@@ -18,6 +18,11 @@ copy_hdrs() {
 	done
 }
 
+copy_crypto() {
+	copy_src crypto/$1 "$2"
+	crypto_subdirs="$crypto_subdirs $1"
+}
+
 cp libssl-openbsd/src/LICENSE COPYING
 cp libssl-openbsd/src/CHANGES ChangeLog
 
@@ -58,15 +63,10 @@ copy_src crypto "cryptlib.h cryptlib.c malloc-wrapper.c mem_clr.c mem_dbg.c cver
 
 crypto_subdirs=
 
-copy_crypt() {
-	copy_src crypto/$1 "$2"
-	crypto_subdirs="$crypto_subdirs $1"
-}
-
-copy_crypt aes "aes_cbc.c aes_core.c aes_misc.c aes_ecb.c aes_cfb.c aes_ofb.c aes_ctr.c
+copy_crypto aes "aes_cbc.c aes_core.c aes_misc.c aes_ecb.c aes_cfb.c aes_ofb.c aes_ctr.c
 	aes_ige.c aes_wrap.c aes_locl.h"
 
-copy_crypt asn1 "a_object.c a_bitstr.c a_utctm.c a_gentm.c a_time.c
+copy_crypto asn1 "a_object.c a_bitstr.c a_utctm.c a_gentm.c a_time.c
 	a_int.c a_octet.c a_print.c a_type.c a_dup.c a_d2i_fp.c a_i2d_fp.c a_enum.c
 	a_utf8.c a_sign.c a_digest.c a_verify.c a_mbstr.c a_strex.c x_algor.c
 	x_val.c x_pubkey.c x_sig.c x_req.c x_attrib.c x_bignum.c x_long.c x_name.c
@@ -78,67 +78,67 @@ copy_crypt asn1 "a_object.c a_bitstr.c a_utctm.c a_gentm.c a_time.c
 	asn1_par.c asn1_lib.c asn1_err.c a_bytes.c a_strnid.c evp_asn1.c asn_pack.c
 	p5_pbe.c p5_pbev2.c p8_pkey.c asn_moid.c a_set.c asn1_locl.h charmap.h"
 
-copy_crypt bf "bf_skey.c bf_ecb.c bf_enc.c bf_cfb64.c bf_ofb64.c bf_locl.h bf_pi.h"
+copy_crypto bf "bf_skey.c bf_ecb.c bf_enc.c bf_cfb64.c bf_ofb64.c bf_locl.h bf_pi.h"
 
-copy_crypt bio "bio_lib.c bio_cb.c bio_err.c bss_mem.c bss_null.c bss_fd.c
+copy_crypto bio "bio_lib.c bio_cb.c bio_err.c bss_mem.c bss_null.c bss_fd.c
 	bss_file.c bss_sock.c bss_conn.c bf_null.c bf_buff.c b_print.c b_dump.c
 	b_sock.c bss_acpt.c bf_nbio.c bss_log.c bss_bio.c bss_dgram.c"
 
-copy_crypt bn "bn_add.c bn_asm.c bn_div.c bn_exp.c bn_lib.c bn_ctx.c bn_mul.c
+copy_crypto bn "bn_add.c bn_asm.c bn_div.c bn_exp.c bn_lib.c bn_ctx.c bn_mul.c
 	bn_mod.c bn_print.c bn_rand.c bn_shift.c bn_word.c bn_blind.c bn_kron.c
 	bn_sqrt.c bn_gcd.c bn_prime.c bn_err.c bn_sqr.c bn_recp.c bn_mont.c
 	bn_mpi.c bn_exp2.c bn_gf2m.c bn_nist.c bn_depr.c bn_const.c bn_x931p.c
 	bn_lcl.h bn_prime.h"
 
-copy_crypt buffer "buffer.c buf_err.c buf_str.c"
+copy_crypto buffer "buffer.c buf_err.c buf_str.c"
 
-copy_crypt cast "c_skey.c c_ecb.c c_enc.c c_cfb64.c c_ofb64.c cast_lcl.h
+copy_crypto cast "c_skey.c c_ecb.c c_enc.c c_cfb64.c c_ofb64.c cast_lcl.h
 	cast_s.h"
 
-copy_crypt cmac "cmac.c cm_ameth.c cm_pmeth.c"
+copy_crypto cmac "cmac.c cm_ameth.c cm_pmeth.c"
 
-#copy_crypt cms "cms_lib.c cms_asn1.c cms_att.c cms_io.c cms_smime.c cms_err.c
+#copy_crypto cms "cms_lib.c cms_asn1.c cms_att.c cms_io.c cms_smime.c cms_err.c
 #	cms_sd.c cms_dd.c cms_cd.c cms_env.c cms_enc.c cms_ess.c cms_pwri.c cms.h cms_lcl.h"
 
-copy_crypt comp "comp_lib.c comp_err.c c_rle.c c_zlib.c"
+copy_crypto comp "comp_lib.c comp_err.c c_rle.c c_zlib.c"
 
-copy_crypt conf "conf_err.c conf_lib.c conf_api.c conf_def.c conf_mod.c
+copy_crypto conf "conf_err.c conf_lib.c conf_api.c conf_def.c conf_mod.c
 	conf_mall.c conf_sap.c conf_def.h"
 
-copy_crypt des "cbc_cksm.c cbc_enc.c cfb64enc.c cfb_enc.c ecb3_enc.c
+copy_crypto des "cbc_cksm.c cbc_enc.c cfb64enc.c cfb_enc.c ecb3_enc.c
 	ecb_enc.c  enc_read.c enc_writ.c fcrypt.c fcrypt_b.c ofb64enc.c ofb_enc.c  pcbc_enc.c
 	qud_cksm.c rand_key.c rpc_enc.c  set_key.c xcbc_enc.c str2key.c  cfb64ede.c
 	ofb64ede.c ede_cbcm_enc.c des_enc.c des_locl.h ncbc_enc.c des_ver.h rpc_des.h spr.h"
 
-copy_crypt dh "dh_asn1.c dh_gen.c dh_key.c dh_lib.c dh_check.c dh_err.c
+copy_crypto dh "dh_asn1.c dh_gen.c dh_key.c dh_lib.c dh_check.c dh_err.c
 	dh_depr.c dh_ameth.c dh_pmeth.c dh_prn.c"
 
-copy_crypt dsa "dsa_gen.c dsa_key.c dsa_lib.c dsa_asn1.c dsa_vrf.c
+copy_crypto dsa "dsa_gen.c dsa_key.c dsa_lib.c dsa_asn1.c dsa_vrf.c
 	dsa_sign.c dsa_err.c dsa_ossl.c dsa_depr.c dsa_ameth.c dsa_pmeth.c
 	dsa_prn.c dsa_locl.h"
 
-copy_crypt dso "dso_dlfcn.c dso_err.c dso_lib.c dso_null.c dso_openssl.c"
+copy_crypto dso "dso_dlfcn.c dso_err.c dso_lib.c dso_null.c dso_openssl.c"
 
-copy_crypt ec "ec_lib.c ecp_smpl.c ecp_mont.c ecp_nist.c ec_cvt.c ec_mult.c
+copy_crypto ec "ec_lib.c ecp_smpl.c ecp_mont.c ecp_nist.c ec_cvt.c ec_mult.c
 	ec_err.c ec_curve.c ec_check.c ec_print.c ec_asn1.c ec_key.c ec2_smpl.c
 	ec2_mult.c ec_ameth.c ec_pmeth.c eck_prn.c ecp_nistp224.c ecp_nistp256.c
 	ecp_nistp521.c ecp_nistputil.c ecp_oct.c ec2_oct.c ec_oct.c ec_lcl.h"
 
-copy_crypt ecdh "ech_lib.c ech_ossl.c ech_key.c ech_err.c ech_locl.h"
+copy_crypto ecdh "ech_lib.c ech_ossl.c ech_key.c ech_err.c ech_locl.h"
 
-copy_crypt ecdsa "ecs_lib.c ecs_asn1.c ecs_ossl.c ecs_sign.c ecs_vrf.c
+copy_crypto ecdsa "ecs_lib.c ecs_asn1.c ecs_ossl.c ecs_sign.c ecs_vrf.c
 	ecs_err.c ecs_locl.h"
 
 # Engine interface is disabled
-copy_crypt engine "eng_err.c eng_lib.c eng_list.c eng_init.c eng_ctrl.c
+copy_crypto engine "eng_err.c eng_lib.c eng_list.c eng_init.c eng_ctrl.c
 	eng_table.c eng_pkey.c eng_fat.c eng_all.c tb_rsa.c tb_dsa.c tb_ecdsa.c
 	tb_dh.c tb_ecdh.c tb_rand.c tb_store.c tb_cipher.c tb_digest.c tb_pkmeth.c
 	tb_asnmth.c eng_openssl.c eng_cnf.c eng_dyn.c eng_rsax.c
 	eng_rdrand.c eng_int.h eng_padlock.c"
 
-copy_crypt err "err.c err_all.c err_prn.c"
+copy_crypto err "err.c err_all.c err_prn.c"
 
-copy_crypt evp "encode.c digest.c evp_enc.c evp_key.c evp_acnf.c e_des.c e_bf.c
+copy_crypto evp "encode.c digest.c evp_enc.c evp_key.c evp_acnf.c e_des.c e_bf.c
 	e_idea.c e_des3.c e_camellia.c e_rc4.c e_aes.c names.c e_xcbc_d.c e_rc2.c
 	e_cast.c e_rc5.c m_null.c m_md4.c m_md5.c m_sha.c m_sha1.c m_wp.c m_dss.c
 	m_dss1.c m_mdc2.c m_ripemd.c m_ecdsa.c p_open.c p_seal.c p_sign.c
@@ -147,73 +147,73 @@ copy_crypt evp "encode.c digest.c evp_enc.c evp_key.c evp_acnf.c e_des.c e_bf.c
 	p5_crpt.c p5_crpt2.c e_old.c pmeth_lib.c pmeth_fn.c pmeth_gn.c m_sigver.c
 	e_aes_cbc_hmac_sha1.c e_rc4_hmac_md5.c evp_locl.h"
 
-copy_crypt hmac "hmac.c hm_ameth.c hm_pmeth.c"
+copy_crypto hmac "hmac.c hm_ameth.c hm_pmeth.c"
 
-copy_crypt idea "i_cbc.c i_cfb64.c i_ofb64.c i_ecb.c i_skey.c idea_lcl.h"
+copy_crypto idea "i_cbc.c i_cfb64.c i_ofb64.c i_ecb.c i_skey.c idea_lcl.h"
 
-copy_crypt krb5 "krb5_asn.c"
+copy_crypto krb5 "krb5_asn.c"
 
-copy_crypt lhash "lhash.c lh_stats.c"
+copy_crypto lhash "lhash.c lh_stats.c"
 
-copy_crypt md4 "md4_dgst.c md4_one.c md4_locl.h"
+copy_crypto md4 "md4_dgst.c md4_one.c md4_locl.h"
 
-copy_crypt md5 "md5_dgst.c md5_one.c md5_locl.h"
+copy_crypto md5 "md5_dgst.c md5_one.c md5_locl.h"
 
-copy_crypt mdc2 "mdc2dgst.c mdc2_one.c"
+copy_crypto mdc2 "mdc2dgst.c mdc2_one.c"
 
-copy_crypt modes "cbc128.c ctr128.c cts128.c cfb128.c ofb128.c gcm128.c
+copy_crypto modes "cbc128.c ctr128.c cts128.c cfb128.c ofb128.c gcm128.c
 	ccm128.c xts128.c modes_lcl.h"
 
-copy_crypt objects "o_names.c obj_dat.c obj_lib.c obj_err.c obj_xref.c obj_xref.h"
+copy_crypto objects "o_names.c obj_dat.c obj_lib.c obj_err.c obj_xref.c obj_xref.h"
 
-copy_crypt ocsp "ocsp_asn.c ocsp_ext.c ocsp_ht.c ocsp_lib.c ocsp_cl.c
+copy_crypto ocsp "ocsp_asn.c ocsp_ext.c ocsp_ht.c ocsp_lib.c ocsp_cl.c
 	ocsp_srv.c ocsp_prn.c ocsp_vfy.c ocsp_err.c"
 
-copy_crypt pem "pem_sign.c pem_seal.c pem_info.c pem_lib.c pem_all.c pem_err.c
+copy_crypto pem "pem_sign.c pem_seal.c pem_info.c pem_lib.c pem_all.c pem_err.c
 	pem_x509.c pem_xaux.c pem_oth.c pem_pk8.c pem_pkey.c pvkfmt.c"
 
-copy_crypt pkcs12 "p12_add.c p12_asn.c p12_attr.c p12_crpt.c p12_crt.c
+copy_crypto pkcs12 "p12_add.c p12_asn.c p12_attr.c p12_crpt.c p12_crt.c
 	p12_decr.c p12_init.c p12_key.c p12_kiss.c p12_mutl.c p12_utl.c p12_npas.c
 	pk12err.c p12_p8d.c p12_p8e.c"
 
-copy_crypt pkcs7 "pk7_asn1.c pk7_lib.c pkcs7err.c pk7_doit.c pk7_smime.c
+copy_crypto pkcs7 "pk7_asn1.c pk7_lib.c pkcs7err.c pk7_doit.c pk7_smime.c
 	pk7_attr.c pk7_mime.c bio_pk7.c"
 
-copy_crypt pqueue "pqueue.c"
+copy_crypto pqueue "pqueue.c"
 
-copy_crypt rand "randfile.c rand_lib.c rand_err.c"
+copy_crypto rand "randfile.c rand_lib.c rand_err.c"
 
-copy_crypt rc2 "rc2_ecb.c rc2_skey.c rc2_cbc.c rc2cfb64.c rc2ofb64.c rc2_locl.h"
+copy_crypto rc2 "rc2_ecb.c rc2_skey.c rc2_cbc.c rc2cfb64.c rc2ofb64.c rc2_locl.h"
 
-copy_crypt rc4 "rc4_enc.c rc4_skey.c rc4_utl.c rc4_locl.h"
+copy_crypto rc4 "rc4_enc.c rc4_skey.c rc4_utl.c rc4_locl.h"
 
-copy_crypt ripemd "rmd_dgst.c rmd_one.c rmd_locl.h rmdconst.h"
+copy_crypto ripemd "rmd_dgst.c rmd_one.c rmd_locl.h rmdconst.h"
 
-copy_crypt rsa "rsa_eay.c rsa_gen.c rsa_lib.c rsa_sign.c rsa_saos.c rsa_err.c
+copy_crypto rsa "rsa_eay.c rsa_gen.c rsa_lib.c rsa_sign.c rsa_saos.c rsa_err.c
 	rsa_pk1.c rsa_ssl.c rsa_none.c rsa_oaep.c rsa_chk.c rsa_null.c rsa_pss.c
 	rsa_x931.c rsa_asn1.c rsa_depr.c rsa_ameth.c rsa_prn.c rsa_pmeth.c
 	rsa_crpt.c rsa_locl.h"
 
-copy_crypt sha "sha_dgst.c sha1dgst.c sha_one.c sha1_one.c sha256.c sha512.c sha_locl.h"
+copy_crypto sha "sha_dgst.c sha1dgst.c sha_one.c sha1_one.c sha256.c sha512.c sha_locl.h"
 
-copy_crypt stack "stack.c"
+copy_crypto stack "stack.c"
 
-copy_crypt ts "ts_err.c ts_req_utils.c ts_req_print.c ts_rsp_utils.c
+copy_crypto ts "ts_err.c ts_req_utils.c ts_req_print.c ts_rsp_utils.c
 	ts_rsp_print.c ts_rsp_sign.c ts_rsp_verify.c ts_verify_ctx.c ts_lib.c
 	ts_conf.c ts_asn1.c"
 
-copy_crypt txt_db "txt_db.c"
+copy_crypto txt_db "txt_db.c"
 
-copy_crypt ui "ui_err.c ui_lib.c ui_openssl.c ui_util.c ui_compat.c ui_locl.h"
+copy_crypto ui "ui_err.c ui_lib.c ui_openssl.c ui_util.c ui_compat.c ui_locl.h"
 
-copy_crypt whrlpool "wp_block.c wp_dgst.c wp_locl.h"
+copy_crypto whrlpool "wp_block.c wp_dgst.c wp_locl.h"
 
-copy_crypt x509 "x509_def.c x509_d2.c x509_r2x.c x509_cmp.c x509_obj.c
+copy_crypto x509 "x509_def.c x509_d2.c x509_r2x.c x509_cmp.c x509_obj.c
 	x509_req.c x509spki.c x509_vfy.c x509_set.c x509cset.c x509rset.c
 	x509_err.c x509name.c x509_v3.c x509_ext.c x509_att.c x509type.c x509_lu.c
 	x_all.c x509_txt.c x509_trs.c by_file.c by_dir.c x509_vpm.c"
 
-copy_crypt x509v3 "v3_bcons.c v3_bitst.c v3_conf.c v3_extku.c v3_ia5.c v3_lib.c
+copy_crypto x509v3 "v3_bcons.c v3_bitst.c v3_conf.c v3_extku.c v3_ia5.c v3_lib.c
 	v3_prn.c v3_utl.c v3err.c v3_genn.c v3_alt.c v3_skey.c v3_akey.c v3_pku.c
 	v3_int.c v3_enum.c v3_sxnet.c v3_cpols.c v3_crld.c v3_purp.c v3_info.c
 	v3_ocsp.c v3_akeya.c v3_pmaps.c v3_pcons.c v3_ncons.c v3_pcia.c v3_pci.c
