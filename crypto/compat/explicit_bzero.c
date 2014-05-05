c82d72e1a13cbf7a78ebc4ea1aef91e675e8db1f
Brent Cook <brent@boundary.com>
2014-05-05 06:42:09 -0500

    replace OpenBSD's explicit_bzero with NetBSD's
    It may be a little cargo cultish (this code is already built into its
    own separate library with -O0), but compilers become more clever all the
    time.

--- crypto/compat/explicit_bzero.c
+++ crypto/compat/explicit_bzero.c
@@ -1,20 +1,16 @@
-/*	$OpenBSD: explicit_bzero.c,v 1.1 2014/01/22 21:06:45 tedu Exp $ */
-/*
- * Public domain.
- * Written by Ted Unangst
- */
+/* $NetBSD: explicit_bzero.c,v 1.1 2012/08/30 12:16:49 drochner Exp $ */
 
-#if !defined(_KERNEL) && !defined(_STANDALONE)
 #include <string.h>
-#else
-#include <lib/libkern/libkern.h>
-#endif
 
 /*
- * explicit_bzero - don't let the compiler optimize away bzero
+ * The use of a volatile pointer guarantees that the compiler
+ * will not optimise the call away.
  */
+void *(* volatile explicit_memset_impl)(void *, int, size_t) = memset;
+
 void
-explicit_bzero(void *p, size_t n)
+explicit_bzero(void *b, size_t len)
 {
-	bzero(p, n);
+
+	(*explicit_memset_impl)(b, 0, len);
 }
