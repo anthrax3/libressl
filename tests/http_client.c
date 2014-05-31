b0db6d7dc6b2aed313af904a01ef1b4af10748f1
Brent Cook <busterb@gmail.com>
2014-05-31 10:11:53 -0500

    Cleanup sample http client
    simplify and fix bugs - the string handling here is really bad

--- tests/http_client.c
+++ tests/http_client.c
@@ -24,73 +24,73 @@ typedef struct {
 	SSL_CTX *ssl_context;
 } connection;
 
-// For this example, we'll be testing on openssl.org
+// For this example, we'll be testing on www.openssl.org
 #define SERVER  "www.openssl.org"
+#define HOST "www.openssl.org"
 #define PORT 443
 
 // Establish a regular tcp connection
 int tcp_connect()
 {
-	int error, handle;
-	struct hostent *host;
-	struct sockaddr_in server;
-
-	host = gethostbyname(SERVER);
-	handle = socket(AF_INET, SOCK_STREAM, 0);
-	if (handle == -1) {
-		perror("Socket");
-		handle = 0;
-	} else {
-		server.sin_family = AF_INET;
-		server.sin_port = htons(PORT);
-		server.sin_addr = *((struct in_addr *)host->h_addr);
-		bzero(&(server.sin_zero), 8);
-
-		error = connect(handle, (struct sockaddr *)&server, sizeof(struct sockaddr));
-		if (error == -1) {
-			perror("Connect");
-			handle = 0;
-		}
+	struct hostent *host = gethostbyname(SERVER);
+	if (!host) {
+		return -1;
 	}
 
-	return handle;
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1) {
+		perror("Socket");
+		return -1;
+	}
+
+	struct sockaddr_in server = {
+		.sin_family = AF_INET,
+		.sin_port = htons(PORT),
+		.sin_addr = *((struct in_addr *)host->h_addr),
+		.sin_zero = { 0 }
+	};
+
+	int error = connect(sock, (struct sockaddr *)&server, sizeof(struct sockaddr));
+	if (error == -1) {
+		close(sock);
+		perror("Connect");
+		return -1;
+	}
+
+	return sock;
 }
 
 // Establish a connection using an SSL layer
 connection *ssl_connect(void)
 {
-	connection *c;
-
-	c = malloc(sizeof(connection));
-	c->ssl_handle = NULL;
-	c->ssl_context = NULL;
+	connection *c = calloc(1, sizeof(connection));
 
 	c->socket = tcp_connect();
-	if (c->socket) {
-		// Register the error strings for libcrypto & libssl
-		SSL_load_error_strings();
-		// Register the available ciphers and digests
-		SSL_library_init();
+	if (c->socket < 0) {
+		free(c);
+		return NULL;
+	}
+	
+	// New context saying we are a client, and using SSL 2 or 3
+	c->ssl_context = SSL_CTX_new(SSLv23_client_method());
+	if (c->ssl_context == NULL) {
+		ERR_print_errors_fp(stderr);
+	}
 
-		// New context saying we are a client, and using SSL 2 or 3
-		c->ssl_context = SSL_CTX_new(SSLv23_client_method());
-		if (c->ssl_context == NULL)
-			ERR_print_errors_fp(stderr);
+	// Create an SSL struct for the connection
+	c->ssl_handle = SSL_new(c->ssl_context);
+	if (c->ssl_handle == NULL) {
+		ERR_print_errors_fp(stderr);
+	}
 
-		// Create an SSL struct for the connection
-		c->ssl_handle = SSL_new(c->ssl_context);
-		if (c->ssl_handle == NULL)
-			ERR_print_errors_fp(stderr);
+	// Connect the SSL struct to our connection
+	if (!SSL_set_fd(c->ssl_handle, c->socket)) {
+		ERR_print_errors_fp(stderr);
+	}
 
-		// Connect the SSL struct to our connection
-		if (!SSL_set_fd(c->ssl_handle, c->socket))
-			ERR_print_errors_fp(stderr);
-
-		// Initiate SSL handshake
-		if (SSL_connect(c->ssl_handle) != 1)
-			ERR_print_errors_fp(stderr);
-	} else {
-		perror("Connect failed");
+	// Initiate SSL handshake
+	if (SSL_connect(c->ssl_handle) != 1) {
+		ERR_print_errors_fp(stderr);
 	}
 
 	return c;
@@ -99,14 +99,16 @@ connection *ssl_connect(void)
 // Disconnect & free connection struct
 void ssl_disconnect(connection * c)
 {
-	if (c->socket)
+	if (c->socket >= 0) {
 		close(c->socket);
+	}
 	if (c->ssl_handle) {
 		SSL_shutdown(c->ssl_handle);
 		SSL_free(c->ssl_handle);
 	}
-	if (c->ssl_context)
+	if (c->ssl_context) {
 		SSL_CTX_free(c->ssl_context);
+	}
 
 	free(c);
 }
@@ -114,55 +116,49 @@ void ssl_disconnect(connection * c)
 // Read all available text from the connection
 char *ssl_read(connection * c)
 {
-	const int readSize = 1024;
 	char *rc = NULL;
-	int received, count = 0;
-	char buffer[1025];
+	int received, rc_len = 0, off = 0;
 
-	if (c) {
-		while (1) {
-			if (!rc)
-				rc = malloc(readSize * sizeof(char) + 1);
-			else
-				rc = realloc(rc, (count + 1) * readSize * sizeof(char) + 1);
+	do {
+		rc_len += 1024;
+		rc = realloc(rc, rc_len);
 
-			received = SSL_read(c->ssl_handle, buffer, readSize);
-			buffer[received] = '\0';
+		received = SSL_read(c->ssl_handle, rc + off, 1024);
 
-			if (received > 0)
-				strcat(rc, buffer);
-
-			if (received < readSize)
-				break;
-			count++;
+		if (received > 0) {
+			off += received;
 		}
-	}
 
+	} while (received == 1024);
+
+	rc[off] = '\0';
 	return rc;
 }
 
 // Write text to the connection
 void ssl_write(connection * c, char *text)
 {
-	if (c)
-		SSL_write(c->ssl_handle, text, strlen(text));
+	SSL_write(c->ssl_handle, text, strlen(text));
 }
 
 // Very basic main: we send GET / and print the response.
 int main(int argc, char **argv)
 {
-	connection *c;
-	char *response;
+	// Register the error strings for libcrypto & libssl
+	SSL_load_error_strings();
+	// Register the available ciphers and digests
+	SSL_library_init();
 
-	c = ssl_connect();
+	connection *c = ssl_connect();
+	if (c) {
+		ssl_write(c, "GET /\r\nHost: " HOST "\r\n\r\n");
 
-	ssl_write(c, "GET /\r\n\r\n");
-	response = ssl_read(c);
+		char *response = ssl_read(c);
+		printf("%s\n", response);
+		free(response);
 
-	printf("%s\n", response);
-
-	ssl_disconnect(c);
-	free(response);
+		ssl_disconnect(c);
+	}
 
 	return 0;
 }
