8052c2f55658a5ce91949aa5fa7fa34cb3f4a7e0
Brent Cook <busterb@gmail.com>
2014-06-27 13:58:24 -0500

    add source synchronization scripts

---
+++ openbsd/sync/update.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# grab the latest openbsd-src
+mkdir -p cvs
+cvsync -c cvs-syncfile
+
+# remove duplicate Attic files
+rm -f cvs/src/lib/libssl/src/doc/crypto/Attic/bio.pod,v \
+	  cvs/src/lib/libssl/src/doc/crypto/Attic/err.pod,v \
+	  cvs/src/lib/libssl/src/doc/crypto/Attic/hmac.pod,v \
+	  cvs/src/lib/libssl/src/doc/crypto/Attic/md5.pod,v \
+	  cvs/src/lib/libssl/src/doc/crypto/Attic/rand.pod,v \
+	  cvs/src/lib/libssl/src/doc/crypto/Attic/rc4.pod,v
+
+# build and checkout the git repository
+rm -fr cvs2git-tmp
+cvs2git --options cvs2git.options --fallback-encoding utf-8
+rm -f openbsd-src.git
+mkdir openbsd-src.git
+(cd openbsd-src.git
+ git init --bare
+ cat ../cvs2git-tmp/git-blob.dat ../cvs2git-tmp/git-dump.dat | git fast-import
+)
+rm -fr openbsd-src
+git clone openbsd-src.git
+
+# grab the directories we're interested in, trim them out
+for dir in libc libssl libcrypto; do
+    echo $dir
+    rm -fr $dir-openbsd $dir-regress-openbsd
+    cp -a openbsd-src ${dir}-openbsd
+    cp -a openbsd-src ${dir}-regress-openbsd
+    (cd ${dir}-openbsd;
+    git filter-branch --prune-empty --subdirectory-filter src/lib/${dir} HEAD
+    git remote add github https://github.com/busterb/${dir}-openbsd.git
+    git push -f github master
+    )
+    (cd ${dir}-regress-openbsd;
+    git filter-branch --prune-empty --subdirectory-filter src/regress/lib/${dir} HEAD
+    git remote add github https://github.com/busterb/${dir}-regress-openbsd.git
+    git push -f github master
+    )
+done
