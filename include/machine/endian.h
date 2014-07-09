6cc645a64eb07d8d91b174ae91587e19c17089ac
Brent Cook <busterb@gmail.com>
2014-07-09 18:07:02 +0200

    redo system compat headers
     * define u_int8_t and u_int32_t for solaris
     * add machine/endian.h compatibility, removing leading _ variants
     * work around the string/strings Posix SUS confusion on solaris
     * always #include_next at the front of the header
     * use LIBCRYPTOCOMPAT_ prefix to avoid conflicts

--- include/machine/endian.h
+++ include/machine/endian.h
@@ -4,24 +4,10 @@
 #ifdef __linux__
 #include <endian.h>
 #else
+#ifdef __sun
+#include <arpa/nameser_compat.h>
+#else
 #include_next <machine/endian.h>
-#endif
-
-#ifndef _BYTE_ORDER
-#ifdef BYTE_ORDER
-#define _BYTE_ORDER BYTE_ORDER
-#endif
-#endif
-
-#ifndef _LITTLE_ENDIAN
-#ifdef LITTLE_ENDIAN
-#define _LITTLE_ENDIAN LITTLE_ENDIAN
-#endif
-#endif
-
-#ifndef _BIG_ENDIAN
-#ifdef BIG_ENDIAN
-#define _BIG_ENDIAN BIG_ENDIAN
 #endif
 #endif
 
--- include/unistd.h
+++ include/unistd.h
@@ -1,16 +1,9 @@
-/*
- * Simple issetugid implementation for Linux/Solaris
- * Public domain
- */
-
 #include_next <unistd.h>
 
-#ifndef LIBBSD_UNISTD_H
-#define LIBBSD_UNISTD_H
+#ifndef LIBCRYPTOCOMPAT_UNISTD_H
+#define LIBCRYPTOCOMPAT_UNISTD_H
 
-__BEGIN_DECLS
 int getentropy(void *buf, size_t buflen);
 int issetugid(void);
-__END_DECLS
 
 #endif
